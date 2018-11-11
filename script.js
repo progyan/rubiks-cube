609992b6ccce93a29b56f58e0afdfd0916ced704
progyan <yanb@protonmail.com>
2018-11-11 18:41:27 +0600

    Added a blue cube

--- script.js
+++ script.js
@@ -1,5 +1,25 @@
 var scene = new THREE.Scene();
-var camera = new THREE.PerspectiveCamera( 75, window.innerWidth/window.innerHeight, 10, 550 );
-var renderer = new THREE.WebGLRenderer({precision: "highp", antialias: true });
+var camera = new THREE.PerspectiveCamera( 75, window.innerWidth/window.innerHeight, 0.1, 550 );
+var renderer = new THREE.WebGLRenderer();
 renderer.setSize( window.innerWidth, window.innerHeight );
 document.body.appendChild( renderer.domElement );
+
+var light = new THREE.PointLight( 0xffffff, 1, 100 );
+light.position.set( 2, 2, 2 );
+scene.add( light );
+var geometry = new THREE.BoxGeometry( 1, 1, 1 );
+var material = new THREE.MeshBasicMaterial( {color: "royalblue"} );
+var cube = new THREE.Mesh( geometry, material );
+scene.add( cube );
+
+camera.position.z = 5;
+
+function animate() {
+	requestAnimationFrame( animate );
+    renderer.render( scene, camera );
+    cube.rotation.x += 0.01;
+    cube.rotation.y += 0.01;
+    cube.rotation.z += 0.01;
+}
+
+animate();
