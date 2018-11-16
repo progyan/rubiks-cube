0800231620fba3bb34e5eecc996f62eb8e3fcebe
progyan <yanb@protonmail.com>
2018-11-17 01:37:43 +0600

    Made cube rotating
    A very big commit

--- script.js
+++ script.js
@@ -10,10 +10,11 @@ const CUBE_COLORS = [
 let scene = new THREE.Scene();
 let camera = new THREE.PerspectiveCamera( 75, window.innerWidth/window.innerHeight, 0.1, 550 );
 let renderer = new THREE.WebGLRenderer();
+let canvas;
 
 function init(){
     renderer.setSize( window.innerWidth, window.innerHeight );
-    document.body.appendChild( renderer.domElement );
+    canvas = document.body.appendChild( renderer.domElement );
 
     scene.background = new THREE.Color( 0xdadada );
 
@@ -40,7 +41,6 @@ function makeCube(x, y, z){
     material.vertexColors = THREE.FaceColors;
     geometry = paintCube(geometry);
     cube = new THREE.Mesh( geometry, material );
-    scene.add( cube );
     cube.position.x = x;
     cube.position.y = y;
     cube.position.z = z;
@@ -48,13 +48,17 @@ function makeCube(x, y, z){
 }
 
 function makeRubik(){
+    let rubik = new THREE.Group();
+    let currentCube;
     for(let i = -1; i < 2; i++){
         for(let j = -1; j < 2; j++){
             for(let k = -1; k < 2; k++){
-                makeCube(i * 1.05, j * 1.05, k * 1.05);
+                currentCube = makeCube(i * 1.05, j * 1.05, k * 1.05);
+                rubik.add(currentCube);
             }
         }
     }
+    return rubik;
 }
 
 function paintCube(geometry){
@@ -62,9 +66,7 @@ function paintCube(geometry){
         let faceColor = CUBE_COLORS[i / 2];
         geometry.faces[i].color = faceColor;
         geometry.faces[i+1].color = faceColor;
-        console.log(faceColor);
     }
-    console.log(geometry.faces);
     return geometry;
 }
 
@@ -73,12 +75,88 @@ function animate() {
     renderer.render( scene, camera );
 }
 
+// MOUSE
+
+let mouseDown = false,
+mouseX = 0,
+mouseY = 0;
+
+function onMouseMove(evt) {
+    if (!mouseDown) {
+        return;
+    }
+
+    evt.preventDefault();
+
+    let deltaX = evt.clientX - mouseX,
+        deltaY = evt.clientY - mouseY;
+    mouseX = evt.clientX;
+    mouseY = evt.clientY;
+    rubik.rotation.x += deltaY / 500;
+    rubik.rotation.y += deltaX / 500;
+}
+
+function onTouchMove(evt){
+    if (!mouseDown) {
+        return;
+    }
+
+    evt.preventDefault();
+
+    let deltaX = evt.touches[0].clientX - mouseX,
+        deltaY = evt.touches[0].clientY - mouseY;
+    mouseX = evt.touches[0].clientX;
+    mouseY = evt.touches[0].clientY;
+    rubik.rotation.x += deltaY / 500;
+    rubik.rotation.y += deltaX / 500;
+}
+
+function onMouseDown(evt) {
+    evt.preventDefault();
+
+    mouseDown = true;
+    mouseX = evt.clientX || evt.touches[0].clientX;
+    mouseY = evt.clientY || evt.touches[0].clientX;
+}
+
+function onMouseUp(evt) {
+    evt.preventDefault();
+
+    mouseDown = false;
+}
+
+function addMouseHandler(canvas) {
+    canvas.addEventListener('mousemove', function (e) {
+        onMouseMove(e);
+    }, false);
+    canvas.addEventListener('touchmove', function (e) {
+        onTouchMove(e);
+    }, false);
+
+    canvas.addEventListener('mousedown', function (e) {
+        onMouseDown(e);
+    }, false);
+    canvas.addEventListener('touchstart', function (e) {
+        onMouseDown(e);
+    }, false);
+
+    canvas.addEventListener('mouseup', function (e) {
+        onMouseUp(e);
+    }, false);
+    canvas.addEventListener('touchend', function (e) {
+        onMouseUp(e);
+    }, false);
+}
+
 // MAIN CODE
 
 init();
 
 camera.position.z = 5;
 
-makeRubik();
+let rubik = makeRubik();
+scene.add(rubik);
+
+addMouseHandler(canvas);
 
 animate();
